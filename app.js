f465ff1e9613478ed6853e2e900e247045eface5
Regisrib <155403194+Regisrib@users.noreply.github.com>
2025-07-24 19:47:25 -0300

    adicionando iamgens nos botoes  e cor

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 let numberOfDrumButtons = document.querySelectorAll('.drum').length
 
 for(let i = 0; i < numberOfDrumButtons; i++) {
-    document.querySelectorAll('button')[i].addEventListener('click', function() {
+    document.querySelectorAll('button')[i].addEventListener('click', function(){
     
-    alert('clicou')    
+    this.style.color = 'pink'
+ 
 })
 
 }
 
 
+/*
+ let audio = new Audio('./sounds/tom-1.mp3');
+    audio.play(); 
+    */
